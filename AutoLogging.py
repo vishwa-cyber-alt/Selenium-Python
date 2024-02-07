ae7b50f9060f8e1a5c22bcca1efdfb7df2d944f4
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2024-02-07 14:46:55 +0530

    AutoLogging

--- AutoLogging.py
+++ AutoLogging.py
@@ -1,1 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Firefox()
+driver.get("https://practicetestautomation.com/practice-test-login/")
+username_input = driver.find_element(By.ID, "username")
+username_input.send_keys("student")
+password=driver.find_element(By.ID,"password")
+password.send_keys("Password123")
+button = driver.find_element(By.ID, "submit")
+button.click();
+
+
+
+
 
